723370aa51ec1ab610bbdf174ba5c5157f9f2221
Avishai Barnoy <23118366+AvishaiBarnoy@users.noreply.github.com>
2025-06-17 21:13:46 +0300

    Fix logging typo in volume energy module

--- modules/energy/volume.py
+++ modules/energy/volume.py
@@ -22,7 +22,7 @@ def calculate_volume_energy(mesh, global_params):
         V0 = (body.target_volume
               if body.target_volume is not None
               else body.options.get("target_volume", 0))
-        logger.debut(f"Default target_volume is 0!")
+        logger.debug(f"Default target_volume is 0!")
         k = body.options.get("volume_stiffness", global_params.volume_stiffness)
 
         volume_energy += 0.5 * k * (V - V0)**2
