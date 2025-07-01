f60dd840ee3e3ea55db2a08a8af00a7b02d6e359
Avishai Barnoy <23118366+AvishaiBarnoy@users.noreply.github.com>
2025-07-01 13:11:53 +0300

    Remove deprecated backtracking gradient descent

--- runtime/steppers/__init__.py
+++ runtime/steppers/__init__.py
@@ -1,6 +1,5 @@
 """Utilities and steppers for runtime optimization."""
 
 from .line_search import backtracking_line_search
-from .backtracking_gradient_descent import BacktrackingGradientDescent
 
-__all__ = ["BacktrackingGradientDescent", "backtracking_line_search"]
+__all__ = ["backtracking_line_search"]
